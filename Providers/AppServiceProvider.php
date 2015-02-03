1b53ce775e124d961e9de06d0f311cb20566b92b
Matthew Reschke <mail@mreschke.com>
2015-02-03 17:43:57 -0600

    Initial commit

---
+++ Providers/AppServiceProvider.php
@@ -0,0 +1,34 @@
+<?php namespace Mrcore\Modules\Auth\Providers;
+
+use Mrcore\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = true;
+
+	/**
+	 * Bootstrap the application services.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		//
+	}
+
+	/**
+	 * Register the application services.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+}
